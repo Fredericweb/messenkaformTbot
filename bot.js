58603abb2bc8eb7aeac5a9d11b232906ede14dca
AsteroBlack <93392469+AsteroBlack@users.noreply.github.com>
2022-11-28 16:58:43 +0000

    MAJ

--- bot.js
+++ bot.js
@@ -3,10 +3,20 @@ const app = express()
 const port = process.env.PORT || 4000
 const cors = require("cors")
 
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
+
+
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf('5330541921:AAGaSB8MPu0vpkht3QxgSNM5XW7mQiVrGPc')
-
 const msg = `
     *******Bienvenue sur zowblazBot********
     /help - pour plus d'info sur les commande du BOT
@@ -17,31 +27,39 @@ const msgHelp = `
     /echo - pour retourner un mot à l'ecran (/echo jeux retourne jeux )
 `;
 const web_link = "https://formappmessenka.herokuapp.com/";
-// const link2 ="https://celebrated-torte-184681.netlify.app/"
-// const link1 = "https://mproweb.uz/YTless/greenMarket/store/"
 
-bot.start((ctx) =>{
-  // bot.telegram.sendPhoto(ctx.chat.id,"https://upload.wikimedia.org/wikipedia/commons/7/77/Google_Images_2015_logo.svg")
+bot.start((ctx) => {
   ctx.reply("Bienvenu sur zowblazo bot", {
     reply_markup: {
       inline_keyboard: [[{ text: "Nouveau formulaire Messenka", web_app: { url: web_link } }
-    ]]
+      ]]
     },
   })
-}
- 
+
+},
 );
 
+app.post('/web-data', async (req, res) => {
+  const {code} = req.body;
+  try {
+      await bot.answerWebAppQuery(queryId, {
+          type: '',
+          id: queryId,
+          title: '',
+          input_message_content: {
+              message_text: code
+          }
+      })
+      return res.status(200).json({});
+  } catch (e) {
+      return res.status(500).json({})
+  }
+})
+
 bot.help((ctx) => {
-    ctx.reply(msgHelp)
-    
+  ctx.reply(msgHelp)
+
 })
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
 
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`)
-})
 bot.launch()
